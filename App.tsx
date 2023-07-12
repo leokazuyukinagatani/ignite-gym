8ba112775f5543c7520fc0683cd14c28ed8655da
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-07-12 20:55:39 -0300

    feat: assets

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar, Text, View } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+export default function App() {
+  const [fonstLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#202024",
+      }}
+    >
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      {fonstLoaded ? <Text>Hello World!</Text> : ""}
+    </View>
+  );
+}
