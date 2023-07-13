220a3cc55282f52f9894304776b5eb6e3ae7826c
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-07-12 22:14:10 -0300

    feat: login screen

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,27 @@ import { Button as ButtonNativeBase, IButtonProps, Text } from "native-base";
 
 type ButtonProps = IButtonProps & {
   title: string;
+  variant?: "solid" | "outline";
 };
-export function Button({ title, ...rest }: ButtonProps) {
+
+export function Button({ title, variant = "solid", ...rest }: ButtonProps) {
   return (
-    <ButtonNativeBase {...rest} 
-    w="full" 
-    h={14} 
-    bg="green.700">
-      <Text>{title}</Text>
+    <ButtonNativeBase
+      {...rest}
+      w="full"
+      h={14}
+      bg={variant === "outline" ? "transparent" : "green.700"}
+      borderWidth={variant === "outline" ? 1 : 0}
+      borderColor="green.500"
+      rounded="sm"
+      _pressed={{ bg: variant === "outline" ? "gray.500" : "green-500" }}
+      _text={{
+        color: variant === "outline" ? "green.500" : "white",
+        fontFamily: "heading",
+        fontSize: "sm",
+      }}
+    >
+      {title}
     </ButtonNativeBase>
   );
 }
