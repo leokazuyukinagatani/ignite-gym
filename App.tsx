c3436ff5d124c98c9d6fb875b7a65f2a6f98e24f
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-07-12 21:14:27 -0300

    feat: theme native base

--- App.tsx
+++ App.tsx
@@ -5,16 +5,18 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
+import { THEME } from "./src/theme";
 export default function App() {
   const [fonstLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fonstLoaded ? <Text>Hello World!</Text> : ""}
+      {!fonstLoaded ? <Text>Hello World!</Text> : <Loading />}
     </NativeBaseProvider>
   );
 }
