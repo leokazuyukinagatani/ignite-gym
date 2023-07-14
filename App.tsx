f96c262427bb54b5c1c4f2910d65922e530aeb72
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-07-13 21:45:32 -0300

    config: routes and icons

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
 import { THEME } from "./src/theme";
-import { SignUp } from "@screens/SignUp";
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fonstLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fonstLoaded ? <SignUp /> : <Loading />}
+      {fonstLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
